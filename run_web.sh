e01d7b815f8cb5e6e8ae9ac3e7fc3d4b066a6f1b
Lonelydeveloper <dhanushkumarganapathy@gmail.com>
2024-01-02 22:39:41 +0530

    dockerizied

--- run_web.sh
+++ run_web.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python manage.py collectstatic --noinput&&python manage.py migrate --noinput&&/usr/local/bin/gunicorn iTicketer.wsgi.application --reload -w 2 -b :8000 --timeout 600
+python manage.py collectstatic --noinput&&python manage.py migrate --noinput&&/usr/local/bin/gunicorn iTicketer.wsgi:application --reload -w 2 -b :8000 --timeout 600
